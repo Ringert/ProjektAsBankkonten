b408c6aa914937efe990c16b96eb46499c7599cf
Markus Ringert <markus.ringert@yahoo.de>
2017-11-23 09:15:09 +0100

    implements listViewKunden selection

--- ProjektAsBankkonto/Benutzeroberflaeche/GUI/MainWindow.xaml.cs
+++ ProjektAsBankkonto/Benutzeroberflaeche/GUI/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 
 using ProjektAsBankkonto.Interfaces.Fachkonzept;
 using ProjektAsBankkonto.Datenhaltung.Model;
+using ProjektAsBankkonto.Datenhaltung.Enums;
 using System.Data;
 
 namespace ProjektAsBankkonto.Benutzeroberflaeche
@@ -30,6 +31,7 @@ namespace ProjektAsBankkonto.Benutzeroberflaeche
             this.Fachkonzept = fachkonzept;
             InitializeComponent();
             this.refreshKunden();
+            this.listViewKunden.SelectionMode = SelectionMode.Single;
         }
 
         private void refreshKunden(object sender, EventArgs e)
@@ -39,17 +41,37 @@ namespace ProjektAsBankkonto.Benutzeroberflaeche
 
         private void refreshKunden()
         {
+            int currentSelectedIndex = listViewKunden.SelectedIndex;
             this.listViewKunden.Items.Clear();
             Dictionary<int, Kunde> kunden = this.Fachkonzept.getAllKunden();
             foreach(KeyValuePair<int, Kunde> item in kunden)
             {
                 this.listViewKunden.Items.Add(item.Value);
             }
-        
+            listViewKunden.SelectedIndex = currentSelectedIndex;
+        }
+
+        private void listViewKunden_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            Kunde kunde = this.listViewKunden.SelectedItem as Kunde;
+            if (kunde != null) {
+                this.kundeNameValue.Text = ((char)kunde.Geschlecht == 'm' ? "Herr" : "Frau") + " " + kunde.Vorname + " " + kunde.Nachname;
+                this.birthValue.Text = kunde.Geburtsdatum.Day + "." + kunde.Geburtsdatum.Month + "." + kunde.Geburtsdatum.Year;
+                this.streetValue.Text = kunde.Strasse;
+                this.cityValue.Text = kunde.Plz + ", " + kunde.Ort;
+                this.landValue.Text = kunde.Land.ToString();
+            } else {
+                this.kundeNameValue.Text = "";
+                this.birthValue.Text = "";
+                this.streetValue.Text = "";
+                this.cityValue.Text = "";
+                this.landValue.Text = "";
+            }
         }
 
         private void listViewKunde_MouseDoubleClick(object sender, RoutedEventArgs e)
         {
+            
             Kunde kunde = ((FrameworkElement)e.OriginalSource).DataContext as Kunde;
             if (kunde != null)
             {
@@ -66,20 +88,17 @@ namespace ProjektAsBankkonto.Benutzeroberflaeche
             kundeEdit.Closed += refreshKunden;
         }
 
-        private void KundeDelButton_Click(object sender, RoutedEventArgs e)
+        private void KundeDeleteButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            Kunde kunde = this.listViewKunden.SelectedItem as Kunde;
+            if (kunde == null)
+            {
+                MessageBox.Show("Sie müssen zuerst einen Kunden auswählen!");
+                return;
+            }
+            this.Fachkonzept.deleteKunde(kunde);
+            this.refreshKunden();
         }
 
-        private void KontoAddButton_Click(object sender, RoutedEventArgs e)
-        {
-            Window kontoEdit = new KontoEdit(this);
-            kontoEdit.Show();
-        }
-
-        private void KontoDelButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
